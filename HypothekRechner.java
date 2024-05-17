31ace22ee2f9c1dbbd2fbcd4ef99bd97b1379ba9
Hasan <alhasan2ramadan@gmail.com>
2024-05-17 15:41:34 +0200

    test update

--- HypothekRechner.java
+++ HypothekRechner.java
@@ -4,5 +4,6 @@ public class HypothekRechner {
     public static void main(String[] args){
         int result = (int)Math.round(Math.random() * 100);
         System.out.println(result);
+        //test
     }
 }
