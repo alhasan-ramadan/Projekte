6c9ee33fbea4dcfe57c7f174bbd89918e5e46c4d
Hasan <alhasan2ramadan@gmail.com>
2024-05-17 16:08:01 +0200

    Testkommentar entfernt

--- HypothekRechner.java
+++ HypothekRechner.java
@@ -4,6 +4,5 @@ public class HypothekRechner {
     public static void main(String[] args){
         int result = (int)Math.round(Math.random() * 100);
         System.out.println(result);
-        //test
     }
 }
