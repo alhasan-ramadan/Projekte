d34a5cd48b79b2790d0707f777b2d6287c753349
Hasan <alhasan2ramadan@gmail.com>
2024-05-17 16:16:26 +0200

    Ordner erstellt und verschoben

--- HypothekRechner.java
+++ HypothekRechner.java
@@ -2,7 +2,6 @@
 
 public class HypothekRechner {
     public static void main(String[] args){
-        int result = (int)Math.round(Math.random() * 100);
-        System.out.println(result);
+        
     }
 }
---
+++ Mini_Projekte/HypothekRechner.java
@@ -0,0 +1,8 @@
+
+
+public class HypothekRechner {
+    public static void main(String[] args){
+        int result = (int)Math.round(Math.random() * 100);
+        System.out.println(result);
+    }
+}
